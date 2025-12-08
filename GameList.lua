987893257ada6fd9260d66e5d6ec283335fe0ea1
Slvfcked <gldpipilupl@gmail.com>
2025-12-08 09:33:50 +0700

    Update GameList.lua

--- GameList.lua
+++ GameList.lua
@@ -7,7 +7,7 @@ elseif placeId == 126509999114328 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/YouRetard/Asthex.cc/refs/heads/main/Loader.lua"))()
 elseif placeId == 4580204640 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/YouRetard/Asthex.cc/refs/heads/main/STK.lua"))()
-elseif placeId == 109983668079237 then
+elseif placeId == 109983668079237 or 96342491571673 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/YouRetard/Asthex.cc/refs/heads/main/SAB.lua"))()
 else
     LocalPlayer:Kick("Game Not Supported :(")
